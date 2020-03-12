b8765a2ccfbce9fc4b6ccf723d395421048cb07d
Michael Romay <amerror@gmail.com>
2020-03-12 15:07:10 -0700

    wrote some model functions

---
+++ __tests__/Model.test.js
@@ -0,0 +1,74 @@
+const Schema = require('../lib/Schema');
+const Model = require('../lib/Model');
+
+describe('Model class', () => {
+  it('creates a new document', () => {
+    const schema = new Schema({
+      name: {
+        type: String,
+        required: true
+      },
+      age: {
+        type: Number,
+        required: true
+      },
+      weight: {
+        type: String
+      }
+    });
+
+    const Dog = new Model('Dog', schema);
+
+    return Dog
+      .create({
+        name: 'spot',
+        age: 5,
+        weight: '20 lbs'
+      })
+      .then(dog => {
+        expect(dog).toEqual({
+          _id: expect.any(String),
+          name: 'spot',
+          age: 5,
+          weight: '20 lbs'
+        });
+      });
+  });
+
+  it('finds by id and updates', () => {
+    const schema = new Schema({
+      name: {
+        type: String,
+        required: true
+      },
+      age: {
+        type: Number,
+        required: true
+      },
+      weight: {
+        type: String
+      }
+    });
+
+    const Dog = new Model('Dog', schema);
+
+    return Dog
+      .create({
+        name: 'spot',
+        age: 5,
+        weight: '20 lbs'
+      })
+      .then(dog => {
+        return Dog
+          .findByIdAndUpdate(dog._id, { name: 'rover' });
+      })
+      .then(updatedDog => {
+        expect(updatedDog).toEqual({
+          _id: expect.any(String),
+          name: 'rover',
+          age: 5,
+          weight: '20 lbs'
+        });
+      });
+  });
+});
